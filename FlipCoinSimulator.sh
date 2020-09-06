6b9979520e0b2926fc7db3e009b782cdf98e94fa
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-06 20:48:23 +0530

    Multiple Time Fliping Coin

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -x
 
-	flip=$((RANDOM%2))
+	read -p "Enter How Many times you want to flip the coin : " flip_for
 
-	if [ $flip -eq 0 ]
-	then
-		echo "Winner is 'Heads'"
-	else
-		echo "Winner is 'Tails'"
-	fi
+	heads=0
+	tails=0
+	while (($flip_for))
+	do
 
+		flip=$((RANDOM%2))
+
+		if [ $flip -eq 0 ]
+		then
+			((heads++))
+		else
+			((tails++))
+		fi
+
+		((flip_for--))
+
+	done
+
+	echo "Head Won $heads Times"
+	echo "Tail Won $tails Times"
