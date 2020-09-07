6595253dc02c17228d448b22da21e4461851b288
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-07 12:06:17 +0530

    If Tie Extra Fliping

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
-#	read -p "Enter How Many times you want to flip the coin : " flip_for
+	read -p "Enter How Many times you want to flip the coin : " flip_for
 
 	heads=0
 	tails=0
-	while True
+	while (($flip_for!=0))
 	do
 
 		flip=$((RANDOM%2))
@@ -21,7 +21,38 @@
 			break
 		fi
 
+		((--flip_for))
+
 	done
 
-	echo "Head Won $heads Times"
-	echo "Tail Won $tails Times"
+	if (($heads==$tails))
+	then
+		while (($heads==$tails))
+		do
+			for ((i=1;i<=2;i++))
+			do
+				flip=$((RANDOM%2))
+				case $flip in
+
+				0) ((heads++))
+				;;
+				1) ((tails++))
+				;;
+				esac
+
+			done
+
+		done
+		echo "heads $heads ,Tails $tails"
+		if [ $heads -gt $tails ]
+		then
+			extra_score=$(($heads-$tails))
+			echo "After Tie Heads Won by $extra_score"
+		else
+			extra_scre=$(($tails-$heads))
+			echo "After Tie Tails Won by $extra_score"
+		fi
+	else
+		echo "Head Won $heads Times"
+		echo "Tail Won $tails Times"
+	fi
